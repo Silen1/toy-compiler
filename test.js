8dcb0836ebddad874d49fc346d205ac6ad3fcafd
周欢 <zhouhuan@souche.com>
2018-12-11 14:23:27 +0800

    formatPrice

--- test.js
+++ test.js
@@ -3,9 +3,43 @@ import parser from './parser'
 import transformer from './transformer'
 import codeGenerator from './codeGenerator'
 
-const tokenizer_res = tokenizer('(add 2 (subtract 4 2) 3)')
+const tokenizer_res = tokenizer('(add 2 (subtract 4 2))')
 const parser_res = parser(tokenizer_res)
 const transformer_res = transformer(parser_res)
-console.log('transformer_res', JSON.stringify(transformer_res, null, 2))
 const codeGenerator_res = codeGenerator(transformer_res)
-console.log('codeGenerator_res', codeGenerator_res)
+// console.log(codeGenerator_res)
+
+function formatPrice (price) {
+  if (!price) {
+    return 0
+  }
+  if (/,/.test(price)) {
+    return price
+  }
+  price = String(price)
+  const intPart = /\./.test(price) ? price.split('.')[0] : price
+  const intPrice = intPart.match(/\d*/g).join('')
+  const HOLDER = 'HOLDER'
+  const holder = price.replace(intPrice, HOLDER)
+  const intPriceArr = intPrice.split('').reverse()
+
+  let res = ''
+  intPriceArr.forEach((item, index) => {
+    if (index % 3 === 0 && index) {
+      res = item + ',' + res
+    } else {
+      res = item + res
+    }
+  })
+  return holder.replace(HOLDER, res)
+}
+
+console.log(formatPrice(2689999))
+console.log(formatPrice('2689999'))
+console.log(formatPrice('2689999元'))
+console.log(formatPrice('裸车价：￥2689999'))
+console.log(formatPrice('优惠：2689999元'))
+console.log(formatPrice('到手价：2689999.99元'))
+console.log(formatPrice('2,689,999'))
+console.log(formatPrice(''))
+console.log(formatPrice())
